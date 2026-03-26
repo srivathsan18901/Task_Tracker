1da1306c69876b4bd43f8e89bcb0e372e887b8f1
srivathsanganesan <srivathsan18901@gmail.com>
2026-03-26 10:14:19 +0530

    26 Changes Add Framer motion

--- task-tracker-frontend/src/components/PageWrapper.jsx
+++ task-tracker-frontend/src/components/PageWrapper.jsx
@@ -3,9 +3,10 @@ import { motion } from "framer-motion";
 export default function PageWrapper({ children }) {
   return (
     <motion.div
-      initial={{ x: -50, opacity: 0 }}
-      animate={{ x: 0, opacity: 1 }}
-      exit={{ x: 50, opacity: 0 }}
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: -20 }}
+      transition={{ duration: 0.3 }}
     >
       {children}
     </motion.div>
